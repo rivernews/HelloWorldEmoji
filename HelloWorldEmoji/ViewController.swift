72c506e5377769dc603b9212a5ba3a207bb80335
Shaung Cheng <shaungcheng@0587364432.wireless.umich.net>
2018-02-01 14:04:30 -0500

    Add dictionary to describe each emoji in message box

--- HelloWorldEmoji/ViewController.swift
+++ HelloWorldEmoji/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showMessage(sender: UIButton){
+        if let emoji = sender.titleLabel?.text {
+            var emojiDict = [
+                "🤣":"rolling on the floor laughing face",
+                "🤪":"goofy face",
+                "🤨": "face with raised eyebow",
+                "🤓": "nerdy face",
+            ]
+            
+            if let explanation = emojiDict[emoji] {
+                // message box
+                let alertController = UIAlertController(title: "Welcome!", message: explanation, preferredStyle: UIAlertControllerStyle.alert)
+                
+                // add action button
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+                
+                // show message box
+                present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
 }
 
